5447f86a864e72ef54ece49c4eb6c1cff5c9e1b7
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-02-14 21:59:41 -0500

    Add post_update method; clears input

---
+++ status_updates/profile.js
@@ -0,0 +1,10 @@
+/* 
+ * profile.js - time-stamped status updates.
+ */
+
+function post_update() {
+    msg = document.getElementById("msg");
+    status = document.getElementById("status");
+    msg.value = "";
+}
+
