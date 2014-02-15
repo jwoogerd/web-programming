17c20ddbb5e84cf51941f0b94ac461f5484aeb21
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-02-14 22:30:03 -0500

    Add posts - in wrong order

--- status_updates/profile.js
+++ status_updates/profile.js
@@ -3,8 +3,19 @@
  */
 
 function post_update() {
-    msg = document.getElementById("msg");
-    status = document.getElementById("status");
+    var msg = document.getElementById("msg");
+    var status = document.getElementById("status");
+    var p = document.createElement("p");
+    var time = new Date();
+    var month = time.getMonth().toString();
+    var date = time.getDate().toString();
+    var year = time.getFullYear().toString();
+    var hour = time.getHours().toString();
+    var mins = time.getMinutes().toString();
+
+    p.innerHTML = month + "/" + date + "/" + year + " " + 
+                  hour + ":" + mins + " " + msg.value;
+    status.appendChild(p);
     msg.value = "";
 }
 
