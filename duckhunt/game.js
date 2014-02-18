ca7514aac394c093c832a19608084e862ff2c70c
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-02-18 07:20:32 -0500

    Add canvas compatibility check
    And shorten up some variable names.

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,44 +1,42 @@
 /*
- * game.js - render a duckhunt game on the html canvas.
+ * game.js - render a Duckhunt game on the HTML canvas.
  */
 
-window.onload = function() {
-    init();
-};
-
 function init() {
-    var game_canvas = document.getElementById("game"),
-        game_context = game_canvas.getContext("2d"),
-        sprite = new Image();
-    sprite.src = "assets/duckhunt.png";
+    var canvas = document.getElementById("game");
+    if (canvas.getContext) {
+        var context = canvas.getContext("2d"),
+            sprite = new Image();
+        sprite.src = "assets/duckhunt.png";
 
-    /* fill background colors */
-    game_context.fillStyle = "#C96A1B";
-    game_context.fillRect(0, 500, 800, 100);
-    game_context.fillStyle = "#87CEEB";
-    game_context.fillRect(0, 0, 800, 500);
+        /* fill background colors */
+        context.fillStyle = "#C96A1B";
+        context.fillRect(0, 500, 800, 100);
+        context.fillStyle = "#87CEEB";
+        context.fillRect(0, 0, 800, 500);
 
-    /* render sprite images */
-    sprite.onload = function () {
-        /* tree */
-        game_context.drawImage(sprite, 0, 270, 95, 180, 
-                                       20, 70, 260, 540);
-        /* ground */
-        game_context.drawImage(sprite, 0, 720, 900, 240, 
-                                       0, 375, 900, 240);
-        /* sniffing dog */
-        game_context.drawImage(sprite, 0, 0, 60, 55, 
-                                       200, 425, 150, 137);
-        /* five birds */
-        game_context.drawImage(sprite, -1, 110, 40, 40, 
-                                       500, 80, 100, 100);
-        game_context.drawImage(sprite, 260, 110, 40, 40, 
-                                       5, 5, 100, 100);
-        game_context.drawImage(sprite, 250, 150, 40, 40, 
-                                       260, 0, 100, 100);
-        game_context.drawImage(sprite, 205, 150, 40, 40, 
-                                       700, 100, 100, 100);
-        game_context.drawImage(sprite, 210, 190, 42, 42, 
-                                       350, 200, 100, 100);
+        /* render sprite images */
+        sprite.onload = function () {
+            /* tree */
+            context.drawImage(sprite, 0, 270, 95, 180, 
+                                      20, 70, 260, 540);
+            /* ground */
+            context.drawImage(sprite, 0, 720, 900, 240, 
+                                      0, 375, 900, 240);
+            /* sniffing dog */
+            context.drawImage(sprite, 0, 0, 60, 55, 
+                                      200, 425, 150, 137);
+            /* five birds */
+            context.drawImage(sprite, -1, 110, 40, 40, 
+                                      500, 80, 100, 100);
+            context.drawImage(sprite, 260, 110, 40, 40, 
+                                      5, 5, 100, 100);
+            context.drawImage(sprite, 250, 150, 40, 40, 
+                                      260, 0, 100, 100);
+            context.drawImage(sprite, 205, 150, 40, 40, 
+                                      700, 100, 100, 100);
+            context.drawImage(sprite, 210, 190, 42, 42, 
+                                      350, 200, 100, 100);
+        }
     };
 }
