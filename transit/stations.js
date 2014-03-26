76feeefb619f712fb17f7ada63a68adf7577686f
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-03-25 20:58:36 -0400

    Map rendering with my location working

--- transit/stations.js
+++ transit/stations.js
@@ -6,24 +6,20 @@ var request = new XMLHttpRequest(),
     lat = 42.373076999999995,
     lng = -71.1027502,
     me = new google.maps.LatLng(lat, lng),
-    options = { zoom : 13,
-                center : me,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-              };
+    options,
+    map;
 
-function init() {
+    function init() {
     request.open("GET", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
     request.onreadystatechange = callback;
     request.send(null);
-
-    map = new google.maps.Map(document.getElementById("trains"), options);
-    getMyLocation();
 }
 
 function callback() {
     if (request.readyState == 4 && request.status == 200) {
         train_data = JSON.parse(request.responseText);
-        //document.getElementById("trains").innerHTML = train_data.line;
+        getMyLocation();
+        renderMap();
         console.log(train_data.line);
     } else if (request.status == 500) {
         document.getElementById("trains").innerHTML = 'Fail...';
@@ -33,10 +29,8 @@ function callback() {
 function getMyLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-        //lat = position.coords.latitude;
-		//lng = position.coords.longitude;
-        console.log(lat);
-		renderMap();
+        lat = position.coords.latitude;
+		lng = position.coords.longitude;
 		});
 	} else {
         alert("Geolocation is not supported by your web browser.  What a shame!");
@@ -44,7 +38,11 @@ function getMyLocation() {
 }
 
 function renderMap() {
-    me = new google.maps.LatLng(lat, lng);
+    options = { zoom : 13,
+                center : me,
+                mapTypeId : google.maps.MapTypeId.ROADMAP
+              };
+    map = new google.maps.Map(document.getElementById("trains"), options);
 
     // Update map and go there...
     map.panTo(me);
