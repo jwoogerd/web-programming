08c0013de516898bc6b1c1a9397337f05a0a2278
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-03-28 09:42:21 -0400

    Add data persistence to save and redisplay updates
    Per lab7 directions. Also fix small bug where minutes < 10
    were not displaying the leading '0'.

--- status_updates/profile.js
+++ status_updates/profile.js
@@ -3,9 +3,9 @@
  */
 
 function post_update() {
-    var msg = document.getElementById("msg"),
-        status = document.getElementById("status"),
-        p = document.createElement("p"),
+    var msg = document.getElementById('msg'),
+        status = document.getElementById('status'),
+        txt = '',
         time = new Date(),
         month = (time.getMonth() + 1).toString(),
         date = time.getDate().toString(),
@@ -13,9 +13,33 @@ function post_update() {
         hour = time.getHours().toString(),
         mins = time.getMinutes().toString();
 
-    p.innerHTML = month + '/' + date + '/' + year + ' ' + 
+    if (mins < 10) mins = '0' + mins;
+
+    txt = month + '/' + date + '/' + year + ' ' + 
                   hour + ':' + mins + ' ' + msg.value;
-    status.insertBefore(p, status.firstChild);
+    status.value = txt;
     msg.value = '';
+    store();
 }
 
+function reload() {
+    var elem = document.getElementById('status'),
+    updates = [],
+    output = '';
+
+    for (var key in localStorage) {
+        updates.push(localStorage[key]);
+    }
+    updates.reverse();
+
+    updates.map(function (txt) {
+        output = output + '<p>' + txt + '</p>\n';
+    });
+    elem.innerHTML = output;
+}
+
+function store() {
+    var txt = document.getElementById('status').value;
+		localStorage[new Date().getTime()] = txt;
+		reload();
+}
